f5cbcf2c7097008044a7006fbdf1768004449450
root <root@c5d67041c2d6>
2022-05-06 07:01:46 +0000

    add api doc

--- ShabbyChat_Client/src/App.js
+++ ShabbyChat_Client/src/App.js
@@ -11,7 +11,7 @@ import myVideo from './asserts/sintel.mp4';
 //local
 /*
 import socketIOClient from "socket.io-client";
-const socket = socketIOClient('localhost:3001', {
+const socket = socketIOClient('localhost:3003', {
     transports: ['websocket']
 });
 */
@@ -140,12 +140,19 @@ class App extends React.Component{
 	const buttonOne = <button type="Submit" onClick={this.submit}>Submit</button>;
 	const buttonTwo = <button onClick={this.submit}>Submitted</button>;
 
-
+ 
 	return (
 	    <div className="App">
+		<ul>
+		    <li><a href="#home">Home</a></li>
+		    <li><a href="http://localhost:3001/api">API</a></li>
+		    <li><a href="#contact">Contact</a></li>
+		    <li><a href="#about">About</a></li>
+		</ul>
 		<header className="App-header">
 		    <img src={logo} className="App-logo" alt="logo" />
 		</header>
+		
 		<div className="App-body"> 
                      <div className="movie">
 		        <div className="moviescreen">
@@ -163,7 +170,7 @@ class App extends React.Component{
 	        <input className="button-container" type="number" id="seconds"/>
                 <button className="button-container" type="button" onClick={this.changeCurrentTime}>set seconds</button>
 		
-	            </div>
+ 	            </div>
 		    <Videochat />
 		    <div className="receive">
                         <p className="message">This is what you received</p>
